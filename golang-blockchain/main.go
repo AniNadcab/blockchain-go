37b2adb22fb2513514f7f6243dd594edcaf77a2a
AniNadcab <anirudhnadcab@gmail.com>
2022-01-27 13:32:29 +0530

    commit

---
+++ golang-blockchain/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+func (chain *BlockChain) addBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.PrevHash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+func main() {
+	chain := InitBlockChain()
+
+	chain.addBlock("First Block After Genesis")
+	chain.addBlock("Second Block After Genesis")
+	chain.addBlock("Third Block After Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Prevoius Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in  Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+
+}
